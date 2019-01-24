d8e91190b176babe3066ec37fa83a4f0c3f40c7e
Anand Neema <anand.neema14@gmail.com>
2019-01-24 16:15:06 +0530

    Person Example

---
+++ Future Proofing/PeopleViewer/People.Library/PeopleRespository.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+
+namespace People.Library
+{
+    public class PeopleRespository
+    {
+        public Person[] GetPeople()
+        {
+            List<Person> persons = new List<Person>();
+            return persons.ToArray();
+        }
+
+        public Person GetPerson(int id)
+        {
+            Person p = new Person();
+            return p;
+        }
+    }
+}
---
+++ Future Proofing/PeopleViewer/PeopleViewer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using People.Library;
+using System;
+using System.Collections.Generic;
+
+namespace PeopleViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PeopleRespository repository = new PeopleRespository();
+
+            //Concrete Approach
+            //This code needs to be change if the underlayingtype is changed
+            Person[] people = repository.GetPeople();
+            List<Person> p = new List<Person>();
+            foreach (var person in people)
+            {
+                p.Add(person);
+            }
+
+            //Abstract Approach
+            //This code will remain intact as far as our return type has IEnumerable implemented
+            IEnumerable<Person> per = repository.GetPeople();
+        }
+    }
+}
