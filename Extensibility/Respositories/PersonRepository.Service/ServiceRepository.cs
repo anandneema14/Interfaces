87d4efb7f253161924881cdd3644281b15e918b1
Anand Neema <anand.neema14@gmail.com>
2019-01-24 18:20:48 +0530

    Extensibility

---
+++ Extensibility/Respositories/PersonRepository.Service/ServiceRepository.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using PersonRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace PersonRepository.Service
+{
+    public class ServiceRepository : IPersonRepository
+    {
+        WebClient client = new WebClient();
+        string baseURI = "http://localhost:63269/api/Values/";
+
+        public void AddPerson(Person newPerson)
+        {
+            
+        }
+
+        public void DeletePerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            string result = client.DownloadString(baseURI + "Get");
+            IEnumerable<Person> people = JsonConvert.DeserializeObject<IEnumerable<Person>>(result);
+            return people;
+        }
+
+        public Person GetPerson()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePerson(int id, Person updatedPerson)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
